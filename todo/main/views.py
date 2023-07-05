046df152c55ebb7a319509304b79003e0fe8910f
vishnu vybhav P <123458279+Vishnuvybhav@users.noreply.github.com>
2023-07-05 09:36:18 +0530

    Add files via upload

---
+++ todo/main/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import *
+from .forms import *
+
+
+def home(request):
+    tasks = Task.objects.all() 
+
+    form = taskform()
+    if request.method == 'POST':
+        form = taskform(request.POST)
+        if form.is_valid():
+           form.save()
+        return redirect('/')   
+    
+    context = {'tasks':tasks,'form':form}
+    return render(request,'list.html',context)
+
+def update_task(request,pk):
+    task = Task.objects.get(id=pk)
+    form = taskform(instance=task)
+    if request.method == 'POST':
+        form = taskform(request.POST,instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+
+    return render(request,'update.html',context)
+
+def delete_task(request,pk):
+    item = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+    context = {'item':item}
+    return render(request,'delete.html',context) 
